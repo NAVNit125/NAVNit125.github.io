14843a8b676ca9c0aed800dedbecf6098c64edc5
Navnit <navnitsingh573@gmail.com>
2023-03-22 14:20:08 +0530

    inital

---
+++ index.js
@@ -0,0 +1,10 @@
+function navnit(){
+    console.log("its working js file");
+}
+
+
+let icon = document.getElementById('micon');
+  icon.onclick = function(){
+  document.body.classList.toggle('dark-theme')
+}
+
